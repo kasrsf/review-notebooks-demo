1130cb48d4c99e62d35c7cfed818bbd20e8c74e8
Kasra Safari <kasra.safari@rubikloud.com>
2019-12-11 15:01:14 -0500

    added folder for cli demo

---
+++ jupytext-cli/notebook.py
@@ -0,0 +1,44 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # Jupytext
+#
+# [Link to repo with introductory Readme](https://github.com/mwouts/jupytext)
+
+
+
+# %matplotlib notebook
+import matplotlib.pyplot as plt
+import numpy as np
+
+# # Heading
+#
+# Some random text. Some random text. Some random text. Some random text. Some random text. Some random text. Some random text.Some random text. Some random text. Some random text. Some random text. Some random text. Some random text. Some random text. Some random text. Some random text.Some random text. Some random text. 
+
+# +
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+
+N = 500
+x = np.random.rand(N)
+y = np.random.rand(N)
+colors = np.random.rand(N)
+area = (30 * np.random.rand(N))**2  # 0 to 15 point radii
+
+plt.scatter(x, y, s=area, c=colors, alpha=0.5)
+plt.show()
+# -
+
+
